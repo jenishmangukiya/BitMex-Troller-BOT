2efd567cfdd9f890eeacb1460059998d3a895a85
Jenish Mangukiya <34957550+jenishmangukiya@users.noreply.github.com>
2019-07-18 18:10:11 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,4 +2,4 @@ setInterval(function(){
 	var d = require('./email_res.js')
 	delete require.cache[require.resolve('./email_res.js')]
 	console.log('--Message has been send!--');
-}, 60000);
+}, 900000);
